2b4c582fb876591d5b11da9e8aff64a45a1d3e44
sauravarya01 <saurav.arya01@nagarro.com>
2023-04-04 16:34:30 +0530

    files

--- src/main/java/com/nagarro/LibraryManagementSystemBackend/repository/LibrarianRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.LibraryManagementSystemBackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagarro.LibraryManagementSystemBackend.model.Librarian;
-
-public interface LibrarianRepository extends JpaRepository<Librarian, String> {
-	public Librarian findByUsernameAndPassword(String username, String password);
-}
